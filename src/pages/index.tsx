140caed14a94c17b0275fe5c345b4c6b84cac3cd
Izarothy <izarothmail@gmail.com>
2023-06-25 16:09:45 +0200

    add og meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,17 @@ export default function Home({ content }: Props) {
       <Head>
         <title>Minecraft Śródziemie</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Minecraft Śródziemie" />
+        <meta
+          property="og:description"
+          content="Społeczność graczy zaprasza na serwer Minecraft RP osadzony w Śródziemiu J. R. R. Tolkiena! Całość rozgrywki odbywa się w Śródziemiu, ale nie trzeba znać historii świata Tolkiena by dołączyć – na razie budujemy miasta i wioski, zaludniamy wsie i drążymy podziemne osady"
+        />
+        <meta
+          property="og:url"
+          content="https://minecraft-srodziemie.vercel.app/"
+        />
+        <meta />
+        <meta />
       </Head>
       <>
         <h1 className="text-3xl font-semibold lg:mb-16 lg:text-5xl">
