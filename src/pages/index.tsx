969bb499fa8e50af5e617f766f7a1e81888596c4
Izarothy <izarothmail@gmail.com>
2023-06-27 17:59:58 +0200

    loading screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,76 +50,81 @@ export default function Home({ content }: Props) {
           property="og:url"
           content="https://minecraft-srodziemie.vercel.app/"
         />
-        <meta />
-        <meta />
       </Head>
-      <>
-        <header className="relative flex h-screen w-full flex-col items-center">
-          <Image
-            src={`/images/${currentImage}.png`}
-            fill
-            alt="Tło"
-            className="object-cover opacity-70"
-          />
-          <span className="relative top-1/3 flex flex-col items-center gap-4">
-            <h1 className="mb-8 text-3xl drop-shadow-lg sm:text-5xl lg:text-7xl">
-              Minecraft Śródziemie
-            </h1>
-            <span className="font-semibold">
-              <span className="relative flex flex-col">
-                <div
-                  className={`${
-                    isTooltipShown ? ` ` : `invisible`
-                  } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm`}
-                >
-                  {hoverText}
-                </div>
-                <button
-                  className="border-0 bg-dark/40"
-                  onMouseEnter={() => {
-                    if (screenWidth < 1024) return;
-                    setTooltipText("Kliknij, by skopiować");
-                    setTooltipShown(true);
-                  }}
-                  onMouseLeave={() => setTooltipShown(false)}
-                  onClick={() => {
-                    setTooltipText("Skopiowano ✔");
-                    navigator.clipboard
-                      .writeText("minecraft-srodziemie.tasrv.com")
-                      .catch(console.log);
+      {currentImage.length > 1 ? (
+        <>
+          <header className="relative flex h-screen w-full flex-col items-center">
+            <Image
+              src={`/images/${currentImage}.png`}
+              fill
+              priority
+              alt="Tło"
+              className="object-cover opacity-70"
+            />
+            <span className="relative top-1/3 flex flex-col items-center gap-4">
+              <h1 className="mb-8 text-3xl drop-shadow-lg sm:text-5xl lg:text-7xl">
+                Minecraft Śródziemie
+              </h1>
+              <span className="font-semibold">
+                <span className="relative flex flex-col">
+                  <div
+                    className={`${
+                      isTooltipShown ? ` ` : `invisible`
+                    } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm`}
+                  >
+                    {hoverText}
+                  </div>
+                  <button
+                    className="border-0 bg-dark/40"
+                    onMouseEnter={() => {
+                      if (screenWidth < 1024) return;
+                      setTooltipText("Kliknij, by skopiować");
+                      setTooltipShown(true);
+                    }}
+                    onMouseLeave={() => setTooltipShown(false)}
+                    onClick={() => {
+                      setTooltipText("Skopiowano ✔");
+                      navigator.clipboard
+                        .writeText("minecraft-srodziemie.tasrv.com")
+                        .catch(console.log);
 
-                    setTooltipShown(true);
-                    setTimeout(() => {
-                      setTooltipShown(false);
-                    }, 1500);
-                  }}
+                      setTooltipShown(true);
+                      setTimeout(() => {
+                        setTooltipShown(false);
+                      }, 1500);
+                    }}
+                  >
+                    minecraft-srodziemie.tasrv.com
+                  </button>
+                </span>
+              </span>
+              <span className="flex gap-2">
+                <a
+                  href="#install"
+                  className="btn text-white transition hover:bg-white hover:font-bold hover:text-dark"
                 >
-                  minecraft-srodziemie.tasrv.com
+                  Instalacja
+                </a>
+                <button className="border-discord bg-discord">
+                  <a href="https://discord.gg/6uddsDd" className="text-white">
+                    Discord
+                  </a>
                 </button>
               </span>
             </span>
-            <span className="flex gap-2">
-              <a
-                href="#install"
-                className="btn text-white transition hover:bg-white hover:font-bold hover:text-dark"
-              >
-                Instalacja
-              </a>
-              <button className="border-discord bg-discord">
-                <a href="https://discord.gg/6uddsDd" className="text-white">
-                  Discord
-                </a>
-              </button>
-            </span>
-          </span>
-        </header>
-        {content && (
-          <article
-            className="px-8 text-justify text-gray-300 md:w-1/2 md:p-0"
-            dangerouslySetInnerHTML={{ __html: content }}
-          ></article>
-        )}
-      </>
+          </header>
+          {content && (
+            <article
+              className="px-8 text-justify text-gray-300 md:w-1/2 md:p-0"
+              dangerouslySetInnerHTML={{ __html: content }}
+            ></article>
+          )}
+        </>
+      ) : (
+        <div className="flex h-screen items-center justify-center text-3xl font-bold">
+          Ładowanie...
+        </div>
+      )}
     </>
   );
 }
