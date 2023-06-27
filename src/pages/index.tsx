1702f097af4da559c57455578189bf9d303abda3
Izarothy <izarothmail@gmail.com>
2023-06-27 22:23:40 +0200

    add a description metatag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,6 +40,10 @@ export default function Home({ content }: Props) {
       <Head>
         <title>Minecraft Śródziemie</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Społeczność graczy zaprasza na serwer Minecraft RP osadzony w Śródziemiu J. R. R. Tolkiena! Całość rozgrywki odbywa się w Śródziemiu, ale nie trzeba znać historii świata Tolkiena by dołączyć – na razie budujemy miasta i wioski, zaludniamy wsie i drążymy podziemne osady"
+        />
         <meta property="og:title" content="Minecraft Śródziemie" />
         <meta
           property="og:description"
@@ -149,3 +153,12 @@ export const getStaticProps = async () => {
     };
   }
 };
+
+/*
+merge to mdx
+
+left - page list
+right - section list - comes from 
+
+
+*/
