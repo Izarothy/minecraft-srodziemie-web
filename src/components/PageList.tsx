3b1072fb6f344b41b3c1f19dd10c046b2fa939ec
Izarothy <izarothmail@gmail.com>
2023-06-28 15:18:59 +0200

    add page list

---
+++ src/components/PageList.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const PageList = () => {
+  return (
+    <aside className="sticky top-10 hidden flex-1 flex-col gap-1 text-[0.95rem] font-semibold md:flex">
+      <Link href="/">
+        <span className=" text-white">Strona główna</span>
+      </Link>
+      <Link href="/mapa">
+        <span className="font-semibold text-[#C0B6AA]">Mapa</span>
+      </Link>
+    </aside>
+  );
+};
+
+export default PageList;
