549e4983ad12844c4e956cc8913815ea65dd8dfc
Izarothy <izarothmail@gmail.com>
2023-06-28 18:05:22 +0200

    dynamically fetch pagelist

--- src/components/PageList.tsx
+++ src/components/PageList.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import { api } from "~/utils/api";
 
 const PageList = () => {
+  const allPages = api.page.getAllPages.useQuery().data;
+
   return (
     <aside className="sticky top-10 hidden flex-1 flex-col gap-1 text-[0.95rem] font-semibold md:flex">
-      <Link href="/">
-        <span className=" text-white">Strona główna</span>
-      </Link>
+      {allPages?.map((page) => {
+        return (
+          <Link
+            href={`/artykuly/${page.slug}`}
+            key={page.title}
+            className={`${page.slug === "index" ? `order-first` : ``}`}
+          >
+            <span className=" text-white">
+              {page.slug === "index" ? "Strona główna" : page.title}
+            </span>
+          </Link>
+        );
+      })}
     </aside>
   );
 };
