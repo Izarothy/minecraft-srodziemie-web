9c5f69a964fd2855213b4c1f5b7c7838457fc61a
Izarothy <izarothmail@gmail.com>
2023-06-28 18:07:25 +0200

    fix pagelist distinction between homepage and rest

--- src/components/PageList.tsx
+++ src/components/PageList.tsx
@@ -8,14 +8,17 @@ const PageList = () => {
   return (
     <aside className="sticky top-10 hidden flex-1 flex-col gap-1 text-[0.95rem] font-semibold md:flex">
       {allPages?.map((page) => {
+        const isHomepage = page.slug === "index";
+        const title = isHomepage ? "Strona główna" : page.title;
+        const path = isHomepage ? "/" : `/artykuly/${page.slug}`;
         return (
           <Link
-            href={`/artykuly/${page.slug}`}
-            key={page.title}
+            href={path}
+            key={title}
             className={`${page.slug === "index" ? `order-first` : ``}`}
           >
             <span className=" text-white">
-              {page.slug === "index" ? "Strona główna" : page.title}
+              {page.slug === "index" ? "Strona główna" : title}
             </span>
           </Link>
         );
