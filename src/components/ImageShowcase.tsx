09deabd689c88b96ced9392c09f144bf9534f036
Izarothy <izarothmail@gmail.com>
2023-06-29 13:17:48 +0200

    rename imageshowcase's title

--- src/components/ImageShowcase.tsx
+++ src/components/ImageShowcase.tsx
@@ -4,7 +4,7 @@ import LocationImage from "./LocationImage";
 const ImageShowcase = () => {
   return (
     <section className="mt-16 w-[90%] lg:w-[70%]">
-      <h2 className="text-center text-4xl">Pokaz</h2>
+      <h2 className="text-center text-3xl sm:text-4xl">Lokacje na serwerze</h2>
       <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 md:gap-x-16 ">
         <LocationImage src="/images/locations/linhir.png" name="Linhir" />
         <LocationImage src="/images/locations/dale.png" name="Dale" />
