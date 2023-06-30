2a31b7525bce874525346c6829999a4329f3e138
Izarothy <izarothmail@gmail.com>
2023-06-30 15:09:57 +0200

    style: stick footer to the bottom of the page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-16 flex h-16 w-full items-center justify-center bg-dark font-bold xl:h-24">
+    <footer className="absolute bottom-0 mt-16 flex h-16 w-full items-center justify-center bg-dark font-bold xl:h-24">
       <a href="https://github.com/Izarothy">Izaroth</a>• 2023
     </footer>
   );
