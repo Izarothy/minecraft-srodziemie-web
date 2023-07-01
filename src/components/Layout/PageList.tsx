2531f05d528501649e572761427c8d10d84fec84
Izarothy <izarothmail@gmail.com>
2023-07-01 13:19:44 +0200

    feat/style: add categories to pagelist, style them

--- src/components/Layout/PageList.tsx
+++ src/components/Layout/PageList.tsx
@@ -6,23 +6,26 @@ const PageList = () => {
   const allPages = api.page.getAllPages.useQuery().data;
 
   return (
-    <aside className="sticky top-10 hidden flex-1 flex-col gap-1 text-[0.95rem] font-semibold md:flex">
-      {allPages?.map((page) => {
-        const isHomepage = page.slug === "index";
-        const title = isHomepage ? "Strona główna" : page.title;
-        const path = isHomepage ? "/" : `/artykuly/${page.slug}`;
-        return (
-          <Link
-            href={path}
-            key={title}
-            className={`${page.slug === "index" ? `order-first` : ``}`}
-          >
-            <span className=" text-white">
-              {page.slug === "index" ? "Strona główna" : title}
-            </span>
-          </Link>
-        );
-      })}
+    <aside className="sticky top-10 hidden flex-1 flex-col gap-4 md:flex">
+      <Link href={"/"} className="order-first ">
+        <span className=" font-bold text-white">Strona Główna</span>
+      </Link>
+      <span className="flex flex-col font-semibold text-white">
+        Artykuły
+        {allPages?.map(({ title, slug }) => {
+          const isHomepage = slug === "index";
+          const path = isHomepage ? "/" : `/artykuly/${slug}`;
+
+          if (!isHomepage)
+            return (
+              <Link href={path} key={title}>
+                <span className="text-sm font-light text-gray-400 hover:text-gray-200">
+                  {title}
+                </span>
+              </Link>
+            );
+        })}
+      </span>
     </aside>
   );
 };
