22b90cdb780ad52a957d42fd803f0de4bc5a2f44
Izarothy <izarothmail@gmail.com>
2023-07-05 16:51:37 +0200

    feat: only display general articles on pagelist

--- src/server/api/routers/pages.ts
+++ src/server/api/routers/pages.ts
@@ -3,7 +3,9 @@ import { getAllPosts } from "~/utils/markdownUtils";
 
 export const pageRouter = createTRPCRouter({
   getAllPages: publicProcedure.query(() => {
-    const allPages = getAllPosts();
+    const allPages = getAllPosts().filter(
+      (page) => page.category === "General"
+    );
 
     return allPages;
   }),
