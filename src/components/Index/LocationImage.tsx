6917b4e0f8e1e8de7f9cdec6cd472d5978d26765
Izarothy <izarothmail@gmail.com>
2023-07-05 16:54:09 +0200

    feat: add links to location pages on imageshowcase

--- src/components/Index/LocationImage.tsx
+++ src/components/Index/LocationImage.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
+import slugify from "~/utils/slugify";
 type Props = {
   src: string;
   name: string;
@@ -12,22 +14,24 @@ const LocationImage = ({ src, name }: Props) => {
       onMouseEnter={() => setImageHovered(true)}
       onMouseLeave={() => setImageHovered(false)}
     >
-      <Image
-        width={500}
-        height={280}
-        className={`${
-          imageHovered ? `opacity-50 transition-all  duration-[400ms]` : ``
-        }  h-full w-full rounded-sm object-cover shadow-lg`}
-        src={src}
-        alt={name}
-      />
-      <span
-        className={`${
-          imageHovered ? `absolute` : `hidden`
-        } left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-4xl font-bold`}
-      >
-        {name}
-      </span>
+      <Link href={`/artykuly/${slugify(name)}`} className="text-inherit">
+        <Image
+          width={500}
+          height={280}
+          className={`${
+            imageHovered ? `opacity-50 transition-all  duration-[400ms]` : ``
+          }  h-full w-full rounded-sm object-cover shadow-lg`}
+          src={src}
+          alt={name}
+        />
+        <span
+          className={`${
+            imageHovered ? `absolute` : `hidden`
+          } left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-4xl font-bold`}
+        >
+          {name}
+        </span>
+      </Link>
     </div>
   );
 };
