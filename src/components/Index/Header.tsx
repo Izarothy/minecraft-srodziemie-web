bec952707ffae25160fbb09555381f05d3b5e61e
Izarothy <izarothmail@gmail.com>
2024-01-22 12:23:32 +0100

    refactor header.tsx

---
+++ src/components/Index/Header.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import ArrowDown from "../Icons/ArrowDown";
+import copyToClipboard from "~/utils/copyToClipboard";
+import discordIcon from "public/images/discord-icon.svg";
+import { chooseImage, imageNames } from "~/utils/chooseImage";
+import handleIPTooltip from "~/utils/handleIPCopy";
+
+type TProps = {
+  screenWidth: number;
+};
+
+const Header = ({ screenWidth }: TProps) => {
+  const [isTooltipShown, setTooltipShown] = useState(false);
+  const [hoverText, setTooltipText] = useState("Kliknij, by skopiować");
+  const [currentImage, setCurrentImage] = useState<string>(imageNames[0]);
+
+  useEffect(() => {
+    const imageInterval = chooseImage(setCurrentImage);
+
+    return () => {
+      clearInterval(imageInterval);
+    };
+  }, []);
+
+  return (
+    <header className=" relative mx-auto flex h-[90vh] w-full flex-col ">
+      <Image
+        src={`/images/${currentImage}.png`}
+        fill
+        priority
+        alt="Tło"
+        quality={100}
+        className="object-cover opacity-50"
+      />
+      <span className="relative top-1/3 flex flex-col items-center gap-4">
+        <h1 className="mb-8 text-3xl drop-shadow-lg sm:text-5xl lg:text-7xl">
+          Minecraft Śródziemie
+        </h1>
+        <span className="font-semibold">
+          <span className="relative flex flex-col">
+            <div
+              className={`${
+                isTooltipShown ? ` ` : `invisible`
+              } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm`}
+            >
+              {hoverText}
+            </div>
+            <button
+              className=" border-0 bg-dark/60 py-2"
+              onMouseEnter={() => {
+                if (screenWidth < 1024) return;
+                setTooltipText("Kliknij, by skopiować");
+                setTooltipShown(true);
+              }}
+              onMouseLeave={() => setTooltipShown(false)}
+              onClick={() => {
+                copyToClipboard("minecraft-srodziemie.tasrv.com");
+                handleIPTooltip(setTooltipText, setTooltipShown);
+              }}
+            >
+              minecraft-srodziemie.tasrv.com
+            </button>
+          </span>
+        </span>
+        <span className="flex items-center gap-2">
+          <a
+            href="#instalacja"
+            className="inline-flex items-center gap-2 border border-yellow-600 bg-dark/70 from-yellow-600/30 to-yellow-600 to-70%  px-4 py-1 font-semibold text-white  hover:bg-gradient-to-br "
+          >
+            Instalacja <ArrowDown />
+          </a>
+          <button className="border-discord ">
+            <a
+              href="https://discord.gg/6uddsDd"
+              className=" flex gap-2 py-1 text-white "
+            >
+              {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+              <Image src={discordIcon} alt="Discord icon" priority />
+              Discord
+            </a>
+          </button>
+        </span>
+      </span>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import ArrowDown from "~/components/Icons/ArrowDown";
 import ImageShowcase from "~/components/Index/ImageShowcase";
 import PageList from "~/components/Layout/PageList";
 import SectionList from "~/components/Layout/SectionList";
-import { imageNames, chooseImage } from "~/utils/chooseImage";
-import copyToClipboard from "~/utils/copyToClipboard";
 import getSectionList from "~/utils/getSectionList";
-import handleIPTooltip from "~/utils/handleIPCopy";
 import { getPostBySlug } from "~/utils/markdownUtils";
 import slugify from "~/utils/slugify";
-import discordIcon from "../../public/images/discord-icon.svg";
+import Header from "~/components/Index/Header";
 
 type Props = {
   content: string;
@@ -21,12 +16,8 @@ type Props = {
 
 export default function Home({ content, sectionList }: Props) {
   const [screenWidth, setScreenWidth] = useState(0);
-  const [hoverText, setTooltipText] = useState("Kliknij, by skopiować");
-  const [currentImage, setCurrentImage] = useState<string>(imageNames[0]);
-  const [isTooltipShown, setTooltipShown] = useState(false);
 
   useEffect(() => {
-    const imageInterval = chooseImage(setCurrentImage);
     setScreenWidth(window.innerWidth);
 
     const handleResize = () => {
@@ -36,7 +27,6 @@ export default function Home({ content, sectionList }: Props) {
 
     return () => {
       window.removeEventListener("resize", handleResize);
-      clearInterval(imageInterval);
     };
   }, []);
 
@@ -59,98 +49,34 @@ export default function Home({ content, sectionList }: Props) {
           content="https://minecraft-srodziemie.vercel.app/"
         />
       </Head>
-      {currentImage ? (
-        <>
-          <header className=" relative mx-auto flex h-[90vh] w-full flex-col ">
-            <Image
-              src={`/images/${currentImage}.png`}
-              fill
-              priority
-              alt="Tło"
-              quality={100}
-              className="object-cover opacity-50"
-            />
-            <span className="relative top-1/3 flex flex-col items-center gap-4">
-              <h1 className="mb-8 text-3xl drop-shadow-lg sm:text-5xl lg:text-7xl">
-                Minecraft Śródziemie
-              </h1>
-              <span className="font-semibold">
-                <span className="relative flex flex-col">
-                  <div
-                    className={`${
-                      isTooltipShown ? ` ` : `invisible`
-                    } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm`}
-                  >
-                    {hoverText}
-                  </div>
-                  <button
-                    className=" border-0 bg-dark/60 py-2"
-                    onMouseEnter={() => {
-                      if (screenWidth < 1024) return;
-                      setTooltipText("Kliknij, by skopiować");
-                      setTooltipShown(true);
-                    }}
-                    onMouseLeave={() => setTooltipShown(false)}
-                    onClick={() => {
-                      copyToClipboard("minecraft-srodziemie.tasrv.com");
-                      handleIPTooltip(setTooltipText, setTooltipShown);
-                    }}
-                  >
-                    minecraft-srodziemie.tasrv.com
-                  </button>
-                </span>
-              </span>
-              <span className="flex items-center gap-2">
-                <a
-                  href="#instalacja"
-                  className="inline-flex items-center gap-2 border border-yellow-600 bg-dark/70 from-yellow-600/30 to-yellow-600 to-70%  px-4 py-1 font-semibold text-white  hover:bg-gradient-to-br "
-                >
-                  Instalacja <ArrowDown />
-                </a>
-                <button className="border-discord ">
-                  <a
-                    href="https://discord.gg/6uddsDd"
-                    className=" flex gap-2 py-1 text-white "
-                  >
-                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-                    <Image src={discordIcon} alt="Discord icon" priority />
-                    Discord
-                  </a>
-                </button>
-              </span>
-            </span>
-          </header>
-          {content && (
-            <main className="relative flex w-[90%] justify-between gap-16 pt-12 lg:w-[80%]">
-              <PageList />
-              <article className="flex-[3] px-8 text-gray-300 md:p-0">
-                <ReactMarkdown
-                  components={{
-                    h2: ({ children }) => {
-                      return <h2 id={slugify(String(children))}>{children}</h2>;
-                    },
-                    a: ({ children, href }) => {
-                      return (
-                        <a target="_blank" href={href}>
-                          {children}
-                        </a>
-                      );
-                    },
-                  }}
-                >
-                  {content}
-                </ReactMarkdown>
-              </article>
-              <SectionList sectionList={sectionList} />
-            </main>
-          )}
-          <ImageShowcase />
-        </>
-      ) : (
-        <div className="flex h-screen items-center justify-center text-3xl font-bold">
-          Ładowanie...
-        </div>
-      )}
+      <>
+        <Header screenWidth={screenWidth} />
+        {content && (
+          <main className="relative flex w-[90%] justify-between gap-16 pt-12 lg:w-[80%]">
+            <PageList />
+            <article className="flex-[3] px-8 text-gray-300 md:p-0">
+              <ReactMarkdown
+                components={{
+                  h2: ({ children }) => {
+                    return <h2 id={slugify(String(children))}>{children}</h2>;
+                  },
+                  a: ({ children, href }) => {
+                    return (
+                      <a target="_blank" href={href}>
+                        {children}
+                      </a>
+                    );
+                  },
+                }}
+              >
+                {content}
+              </ReactMarkdown>
+            </article>
+            <SectionList sectionList={sectionList} />
+          </main>
+        )}
+        <ImageShowcase />
+      </>
     </>
   );
 }
