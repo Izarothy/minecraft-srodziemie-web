150fcd9e97e50a4a5c3b7d36ba7e36eca0915486
Izarothy <izarothmail@gmail.com>
2024-03-02 14:51:11 +0100

    update header.tsx look

--- src/components/Index/Header.tsx
+++ src/components/Index/Header.tsx
@@ -5,6 +5,8 @@ import copyToClipboard from "~/utils/copyToClipboard";
 import discordIcon from "public/images/discord-icon.svg";
 import { chooseImage, imageNames } from "~/utils/chooseImage";
 import handleIPTooltip from "~/utils/handleIPCopy";
+import { Button } from "../ui/button";
+import ClipboardCopy from "~/components/Icons/ClipboardCopy";
 
 const Header = () => {
   const [isTooltipShown, setTooltipShown] = useState(false);
@@ -27,23 +29,43 @@ const Header = () => {
         priority
         alt="Tło"
         quality={100}
-        className="object-cover opacity-50"
+        className="object-cover opacity-40"
       />
       <span className="relative top-1/3 flex flex-col items-center gap-4">
         <h1 className="mb-8 text-3xl drop-shadow-lg sm:text-5xl lg:text-7xl">
           Minecraft Śródziemie
         </h1>
-        <span className="font-semibold">
+        <p className="max-w-xl text-center  text-gray-200">
+          Serwer Minecraft osadzony w Śródziemiu! Jeśli jesteś fanem Tolkiena,
+          znajdziesz tu coś dla siebie.
+        </p>
+        <span className="flex items-center gap-8">
+          <a href="#instalacja">
+            <Button className="inline-flex items-center gap-2 border border-gray-600 bg-purple-700 hover:bg-purple-600">
+              Instalacja
+              <ArrowDown />
+            </Button>
+          </a>
+
+          <a href="https://discord.gg/6uddsDd">
+            <Button className="inline-flex gap-2 border border-gray-500 bg-[#7289da] hover:bg-[#8ba0e9]">
+              {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+              <Image src={discordIcon} alt="Discord icon" priority />
+              Discord
+            </Button>
+          </a>
+        </span>
+        <span className="mt-0 font-semibold">
           <span className="relative flex flex-col">
             <div
               className={`${
                 isTooltipShown ? ` ` : `invisible`
-              } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm`}
+              } mx-auto mb-2 flex-1 rounded-md bg-dark/80 px-6 py-1 text-center text-sm font-normal`}
             >
               {hoverText}
             </div>
             <button
-              className=" border-0 bg-dark/60 py-2"
+              className=" inline-flex gap-2 border-0 bg-dark/20 py-2 text-xs font-normal text-gray-300"
               onMouseEnter={() => {
                 const screenWidth = window.innerWidth;
                 if (screenWidth < 1024) return;
@@ -57,26 +79,9 @@ const Header = () => {
               }}
             >
               minecraft-srodziemie.tasrv.com
+              <ClipboardCopy />
             </button>
           </span>
-        </span>
-        <span className="flex items-center gap-2">
-          <a
-            href="#instalacja"
-            className="inline-flex items-center gap-2 border border-yellow-600 bg-dark/70 from-yellow-600/30 to-yellow-600 to-70%  px-4 py-1 font-semibold text-white  hover:bg-gradient-to-br "
-          >
-            Instalacja <ArrowDown />
-          </a>
-          <button className="border-discord ">
-            <a
-              href="https://discord.gg/6uddsDd"
-              className=" flex gap-2 py-1 text-white "
-            >
-              {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-              <Image src={discordIcon} alt="Discord icon" priority />
-              Discord
-            </a>
-          </button>
         </span>
       </span>
     </header>
